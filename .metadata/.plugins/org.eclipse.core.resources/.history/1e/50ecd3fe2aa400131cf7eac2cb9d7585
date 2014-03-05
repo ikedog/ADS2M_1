c03cfa41f3956909cf788722effe36a4c36f80ca
Henrique Tein <henrique.tein9@gmail.com>
2014-03-05 02:57:56 -0300

    hello world para hello user

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1e/50ecd3fe2aa400131cf7eac2cb9d7585
@@ -0,0 +1,12 @@
+package com.senac.exemples;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
--- HelloWorld/src/com/senac/exemples/Principal.java
+++ HelloWorld/src/com/senac/exemples/Principal.java
@@ -1,11 +1,15 @@
 package com.senac.exemples;
-
+import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello World");
+		Scanner e = new Scanner(System.in);
+		String user;
+		System.out.println("Qual seu nome?");
+		user = e.nextLine();
+		
+		System.out.println("Hello "+user);
 		
 	}
 
